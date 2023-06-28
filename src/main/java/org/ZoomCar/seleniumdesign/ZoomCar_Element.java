afda2ba958d20b9b829145751edca8ed517eb5eb
manojrs0410 <manojkumar.selvaraj@ideas2it.com>
2023-06-28 23:19:23 +0530

    index on develop: dd64de8 fix

--- src/main/java/org/ZoomCar/seleniumdesign/ZoomCar_Element.java
+++ src/main/java/org/ZoomCar/seleniumdesign/ZoomCar_Element.java
@@ -1,15 +1,25 @@
 package org.ZoomCar.seleniumdesign;
 
+import java.util.List;
+
 import org.openqa.selenium.WebElement;
 
 public interface ZoomCar_Element {
 	
-	public void click (WebElement element);
-	
-	public void enterKey (WebElement element);
+	public void click (WebElement element, String desc);
 	
 	public void enterText (WebElement element, String data);
 	
-	public void mouseOver (WebElement element);
+	public void mouseHover (WebElement element);
+	
+	public void startAndEndSlider(WebElement elementStart, WebElement elementEnd);
+	
+	public void secondLocation(List<WebElement> element);
+	
+	public String newlyAddedCars(List<WebElement> elements);
+	
+	public void highRatedCar(List<WebElement> element);
+	
+	public void listOfImages(List<WebElement> element);
 	
 }
